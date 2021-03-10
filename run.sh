23ed7ee26742b94fc0a7a2ed4d7a9d472979236e
Marcel Mendes <marcelmendes@hotmail.co.uk>
2021-03-10 11:16:41 +0000

    remove sendgrid key

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ read emailAddress
 echo "Please enter Pact broker API Token:"
 read pactBrokerToken
 
-export RPA_SENDGRID_API_KEY=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
+export RPA_SENDGRID_API_KEY=
 export RPA_FROM_EMAIL=civilunspecified@gmail.com
 export RPA_PACT_BROKER_URL='https://civil-damages-claims.pactflow.io/'
 export RPA_PACT_BROKER_TOKEN=$pactBrokerToken

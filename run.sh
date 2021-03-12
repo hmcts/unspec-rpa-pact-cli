38949b31d480b60074bf40995e835aa7ef8d8698
devmarcy <marcelmendes@hotmail.co.uk>
2021-03-12 15:23:00 +0000

    changes to run.sh and pushing new package.json

--- run.sh
+++ run.sh
@@ -9,14 +9,13 @@ read emailAddress
 echo "Please enter Pact broker API Token:"
 read pactBrokerToken
 
-export RPA_SENDGRID_API_KEY=
+export RPA_SENDGRID_API_KEY=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
 export RPA_FROM_EMAIL=civilunspecified@gmail.com
 export RPA_PACT_BROKER_URL='https://civil-damages-claims.pactflow.io/'
 export RPA_PACT_BROKER_TOKEN=$pactBrokerToken
 
 export RPA_CONSUMER_VERSION_TAG=$versionTag
 export RPA_TO_EMAIL=$emailAddress
-export NODE_ENV=production
 
 #Hotpatching the library (https://tools.hmcts.net/jira/browse/CMC-1275)
 #This needs to be removed once the client has been fixed (https://github.com/pact-foundation/pact-ruby-standalone/issues/55)

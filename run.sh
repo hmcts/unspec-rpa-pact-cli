c13afba3aa02b611cf29674861a43acf7b32edb3
devmarcy <marcelmendes@hotmail.co.uk>
2021-03-15 17:26:43 +0000

    changes to make cli work on windows

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ read emailAddress
 echo "Please enter Pact broker API Token:"
 read pactBrokerToken
 
-export RPA_SENDGRID_API_KEY=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
+export RPA_SENDGRID_API_KEY=
 export RPA_FROM_EMAIL=civilunspecified@gmail.com
 export RPA_PACT_BROKER_URL='https://civil-damages-claims.pactflow.io/'
 export RPA_PACT_BROKER_TOKEN=$pactBrokerToken
@@ -26,9 +26,6 @@ yarn install
 
 if [ -e ./node_modules/@pact-foundation/pact-node/standalone/darwin-1.88.3/pact/lib/ruby/lib ]; then
   cp ./certificate-bundle/ca-bundle.crt ./node_modules/@pact-foundation/pact-node/standalone/darwin-1.88.3/pact/lib/ruby/lib
-else
-#  the path on the windows
-  cp ./certificate-bundle/ca-bundle.crt ./node_modules/@pact-foundation/pact-node/standalone/win32-1.88.3/pact/lib/ruby/lib
 fi
 
 yarn test:pact
